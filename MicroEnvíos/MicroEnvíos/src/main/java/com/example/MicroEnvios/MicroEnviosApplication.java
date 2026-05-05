75c4892cf31a0ce86aa25abeb35d90e3e717515b
dieriverar28 <die.riverar@duocuc.cl>
2026-05-05 13:07:44 -0400

    MicroEnvio

---
+++ MicroEnvíos/MicroEnvíos/src/main/java/com/example/MicroEnvios/MicroEnviosApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroEnvios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroEnviosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroEnviosApplication.class, args);
+	}
+
+}
