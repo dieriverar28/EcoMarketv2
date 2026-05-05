8a0f87d756f8b189aba9e539abfe87aea1775088
dieriverar28 <die.riverar@duocuc.cl>
2026-05-05 13:14:38 -0400

    MicroVentaPago

---
+++ MicroVentaPagos/MicroVentaPagos/src/main/java/com/example/MicroVentaPagos/MicroVentaPagosApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroVentaPagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroVentaPagosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroVentaPagosApplication.class, args);
+	}
+
+}
---
+++ MicroVentaPagos/MicroVentaPagos/src/test/java/com/example/MicroVentaPagos/MicroVentaPagosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroVentaPagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroVentaPagosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
