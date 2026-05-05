a89c974596eb7323a0343ca144e7315d31f80228
dieriverar28 <die.riverar@duocuc.cl>
2026-05-05 13:11:10 -0400

    MicroPostVenta

---
+++ MicroPostventa/MicroPostventa/src/main/java/com/example/MicroPostventa/MicroPostventaApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroPostventa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroPostventaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroPostventaApplication.class, args);
+	}
+
+}
---
+++ MicroPostventa/MicroPostventa/src/test/java/com/example/MicroPostventa/MicroPostventaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroPostventa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroPostventaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
