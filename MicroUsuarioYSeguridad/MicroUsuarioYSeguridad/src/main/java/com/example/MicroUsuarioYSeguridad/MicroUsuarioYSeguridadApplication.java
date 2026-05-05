3b7cb9e23acb6315c3f28c7e3e1e6d2440d725dc
dieriverar28 <die.riverar@duocuc.cl>
2026-05-05 13:13:31 -0400

    MicroUsuarioSeguridad

---
+++ MicroUsuarioYSeguridad/MicroUsuarioYSeguridad/src/main/java/com/example/MicroUsuarioYSeguridad/MicroUsuarioYSeguridadApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroUsuarioYSeguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroUsuarioYSeguridadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroUsuarioYSeguridadApplication.class, args);
+	}
+
+}
---
+++ MicroUsuarioYSeguridad/MicroUsuarioYSeguridad/src/test/java/com/example/MicroUsuarioYSeguridad/MicroUsuarioYSeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroUsuarioYSeguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroUsuarioYSeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
