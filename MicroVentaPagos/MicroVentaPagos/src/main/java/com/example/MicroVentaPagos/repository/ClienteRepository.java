aab7e67909817eaa746c5691f4dde15eb3d8a3f8
belenvelasco <138613417+belenvelasco@users.noreply.github.com>
2026-05-07 15:36:55 -0400

    micro ventaServiceModeloRepositoryServiceController

---
+++ MicroVentaPagos/MicroVentaPagos/src/main/java/com/example/MicroVentaPagos/repository/ClienteRepository.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.EcoMarketSPA.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+    
+ @Query("SELECT c FROM Cliente c")
+    List<Cliente> obtenerClientes();
+
+    @Query("SELECT c FROM Cliente c WHERE c.id_cliente = :id_cliente")
+    Cliente buscarCliente(int id_cliente);
+}
